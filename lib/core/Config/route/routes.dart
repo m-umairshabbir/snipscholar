677fc827a4321e86a9954cb70be48ecab99b23ea
m-umairshabbir <theumair3191@gmail.com>
2025-04-07 18:23:30 +0800

    initial

---
+++ lib/core/Config/route/routes.dart
@@ -0,0 +1,8 @@
+class Routes{
+  static Route<dynamic> onGenerateRoute(RouteSettings setting){
+    switch(setting.name){
+      case RouteNames.splashScreen:
+        return
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:snipscholar/core/presentation/test_screen.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -11,20 +15,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'SnipScholar',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Home(),
+      home: const TestScreen(),
     );
   }
 }
-class Home extends StatelessWidget {
-  const Home({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
